5202aec78262a0464e9bad6bd77dc8e9869ad851
goodcode200 <goodcode200@gmail.com>
2019-05-21 22:22:41 +0100

    Implement base64 encoding, JSON, and error handling

--- Program.cs
+++ Program.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 
 namespace endiffo
 {
+    public struct Constants
+    {
+        public const string HOSTS_FILE = "/etc/hosts";
+        public const string ENVIRON_VAR_COMMAND = "printenv";
+        public const string DEFAULT_SNAPSHOT_FILE = "snapshot.endiffo";
+    }
+
+    public struct SystemSnapshot
+    {
+        // Environment variables.
+        [JsonProperty]
+        public readonly string EnvBase64;
+
+        // Contents of HOSTS_FILE.
+        [JsonProperty]
+        public readonly string HostsBase64;
+
+        public SystemSnapshot(string envBase64, string hostsBase64)
+        {
+            EnvBase64 = envBase64;
+            HostsBase64 = hostsBase64;
+        }
+    };
+
     class Program
     {
+        // Reference: https://stackoverflow.com/questions/11743160/how-do-i-encode-and-decode-a-base64-string
+        static string EncodeToBase64(string unencoded)
+        {
+            return Convert.ToBase64String(Encoding.UTF8.GetBytes(unencoded));
+        }
+
         // static string ExecViaBash()
         // {
         //     var process = new Process()
@@ -26,9 +58,9 @@ namespace endiffo
         //     return result;
         // }
 
-        // Works on Windows and Linux
-        // Note that this is an insecure way to run an application.
-        // An alternative is discussed here:
+        // Returns null in case of an error.
+        // Written to work on Windows and Linux
+        // Note that this is an insecure way to run an application. An alternative is discussed here:
         // https://stackoverflow.com/questions/48296629/launch-external-process-exe-from-asp-net-core-app
         static string PrintEnv()
         {
@@ -36,7 +68,7 @@ namespace endiffo
             {
                 StartInfo = new ProcessStartInfo
                 {
-                    FileName = "printenv",
+                    FileName = Constants.ENVIRON_VAR_COMMAND,
                     RedirectStandardOutput = true,
                     UseShellExecute = false,
                     CreateNoWindow = true,
@@ -45,22 +77,42 @@ namespace endiffo
 
             process.Start();
             string result = process.StandardOutput.ReadToEnd();
+
+            // TODO The application could potentially freeze here.
             process.WaitForExit();
             return result;
         }
 
+        // TODO If the hosts file is unexpectedly large, we might want to consider streaming it,
+        // in which case this function signature will change.
         static string GetHosts()
         {
-            return File.ReadAllText("/etc/hosts");
+            return File.ReadAllText(Constants.HOSTS_FILE);
         }
 
         static void Main(string[] args)
         {
-            string res = PrintEnv();
-            File.WriteAllText("snapshot.endiffo", res);
+            try
+            {
+                var snapshot = new SystemSnapshot (
+                    envBase64: EncodeToBase64(PrintEnv()),
+                    hostsBase64: EncodeToBase64(GetHosts())
+                );
 
-            string hosts = GetHosts();
-            File.WriteAllText("hosts.txt", hosts);
+                File.WriteAllText(
+                    Constants.DEFAULT_SNAPSHOT_FILE,
+                    JsonConvert.SerializeObject(snapshot)
+                );
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine (
+                    "An error occurred and the application had to terminate." + Environment.NewLine
+                    + "Error text: " + ex.Message + Environment.NewLine
+                    + "Inner exception: " + ex.InnerException + Environment.NewLine
+                    + "Stack trace: " + ex.StackTrace
+                );
+            }
         }
     }
 }
