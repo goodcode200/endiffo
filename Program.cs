160e1df1e541a5fa7c449b06ffe682e5851c1c66
goodcode200 <goodcode200@gmail.com>
2019-05-21 20:13:29 +0100

    Windows compatibility

--- Program.cs
+++ Program.cs
@@ -26,9 +26,29 @@ namespace endiffo
             return result;
         }
 
+        static string WindowsPrintEnv()
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "printenv",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            string res = LinuxPrintEnv();
+            //string res = LinuxPrintEnv();
+            string res = WindowsPrintEnv();
             File.WriteAllText("snapshot.endiffo", res);
         }
     }
