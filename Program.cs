3e1dc018f847c107c9363650e4cf9179b345dc6a
goodcode200 <goodcode200@gmail.com>
2019-05-21 21:34:37 +0100

    Implement reading of hosts file on Linux

--- Program.cs
+++ Program.cs
@@ -6,27 +6,31 @@ namespace endiffo
 {
     class Program
     {
-        static string LinuxPrintEnv()
-        {
-            var process = new Process()
-            {
-                StartInfo = new ProcessStartInfo
-                {
-                    FileName = "/bin/bash",
-                    Arguments = "-c \"printenv\"",
-                    RedirectStandardOutput = true,
-                    UseShellExecute = false,
-                    CreateNoWindow = true,
-                }
-            };
+        // static string ExecViaBash()
+        // {
+        //     var process = new Process()
+        //     {
+        //         StartInfo = new ProcessStartInfo
+        //         {
+        //             FileName = "/bin/bash",
+        //             Arguments = "-c \"printenv\"",
+        //             RedirectStandardOutput = true,
+        //             UseShellExecute = false,
+        //             CreateNoWindow = true,
+        //         }
+        //     };
 
-            process.Start();
-            string result = process.StandardOutput.ReadToEnd();
-            process.WaitForExit();
-            return result;
-        }
+        //     process.Start();
+        //     string result = process.StandardOutput.ReadToEnd();
+        //     process.WaitForExit();
+        //     return result;
+        // }
 
-        static string WindowsPrintEnv()
+        // Works on Windows and Linux
+        // Note that this is an insecure way to run an application.
+        // An alternative is discussed here:
+        // https://stackoverflow.com/questions/48296629/launch-external-process-exe-from-asp-net-core-app
+        static string PrintEnv()
         {
             var process = new Process()
             {
@@ -45,11 +49,18 @@ namespace endiffo
             return result;
         }
 
+        static string GetHosts()
+        {
+            return File.ReadAllText("/etc/hosts");
+        }
+
         static void Main(string[] args)
         {
-            //string res = LinuxPrintEnv();
-            string res = WindowsPrintEnv();
+            string res = PrintEnv();
             File.WriteAllText("snapshot.endiffo", res);
+
+            string hosts = GetHosts();
+            File.WriteAllText("hosts.txt", hosts);
         }
     }
 }
