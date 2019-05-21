4c9dca016391ae3ad3c5a98caeaa2c19403f08d7
goodcode200 <goodcode200@gmail.com>
2019-05-21 18:43:16 +0100

    new console app

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace endiffo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
