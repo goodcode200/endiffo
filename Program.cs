21bd5e9c5f7403f44de93eba48371c997b6752b0
goodcode200 <goodcode200@gmail.com>
2019-05-21 20:12:15 +0100

    Linux environment variables

--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 
 namespace endiffo
 {
     class Program
     {
+        static string LinuxPrintEnv()
+        {
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "/bin/bash",
+                    Arguments = "-c \"printenv\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+                }
+            };
+
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            return result;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string res = LinuxPrintEnv();
+            File.WriteAllText("snapshot.endiffo", res);
         }
     }
 }
